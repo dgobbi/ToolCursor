8b4ae90e3f09c84b1cc4a26a6de4d38bd51e08b1
David Gobbi <david.gobbi@gmail.com>
2009-12-09 21:51:53 +0000

    ENH: Add the spinner cursor.

--- vtkSurfaceCursor.cxx
+++ vtkSurfaceCursor.cxx
@@ -52,7 +52,7 @@
 #include "vtkWarpTo.h"
 #include "vtkTransform.h"
 
-vtkCxxRevisionMacro(vtkSurfaceCursor, "$Revision: 1.7 $");
+vtkCxxRevisionMacro(vtkSurfaceCursor, "$Revision: 1.8 $");
 vtkStandardNewMacro(vtkSurfaceCursor);
 
 //----------------------------------------------------------------------------
@@ -461,7 +461,7 @@ void vtkSurfaceCursor::ComputePosition()
     }
   else if (this->State & VTK_SCURSOR_MOVEABLE)
     {
-    this->SetShape(VTK_SCURSOR_ROCKER);
+    this->SetShape(VTK_SCURSOR_SPINNER);
     }
   else
     {
@@ -1123,7 +1123,129 @@ vtkDataSet *vtkSurfaceCursor::MakePusherShape()
 //----------------------------------------------------------------------------
 vtkDataSet *vtkSurfaceCursor::MakeSpinnerShape()
 {
-  return vtkSurfaceCursor::MakeMoverShape(0);
+  vtkPoints *points = vtkPoints::New();
+  vtkCellArray *polys = vtkCellArray::New();
+
+  int color = 0;
+
+  static double coords[7][3] = {
+    { 4, 2, 0 }, { 15, 2, 0 }, { 14, 8, 0 }, { 24, 0.01, 0 },
+    { 14, -8, 0 }, { 15, -2, 0 }, { 4, -2, 0 },
+  };
+
+  static vtkIdType polyIds[] = {
+    7, 0, 1, 2, 3, 4, 5, 6,
+  };
+
+  for (int i = 0; i < 7; i++)
+    {
+    points->InsertNextPoint(coords[i]);
+    }
+  polys->InsertNextCell(polyIds[0], &polyIds[1]);
+
+  vtkPolyData *arrow = vtkPolyData::New();
+  arrow->SetPoints(points);
+  points->Delete();
+  arrow->SetPolys(polys);
+  polys->Delete();
+
+  vtkImplicitModeller *modeller = vtkImplicitModeller::New();
+  modeller->SetInput(arrow);
+  modeller->SetSampleDimensions(32, 16, 8);
+
+  vtkContourFilter *contour = vtkContourFilter::New();
+  contour->SetInputConnection(modeller->GetOutputPort());
+  contour->SetValue(0, 0.5);
+
+  // The image is inside-out, and so is the contour
+  vtkReverseSense *reverse = vtkReverseSense::New();
+  reverse->SetInputConnection(contour->GetOutputPort());
+  reverse->ReverseCellsOn();
+
+  vtkWarpTo *warp = vtkWarpTo::New();
+  warp->SetInputConnection(reverse->GetOutputPort());
+  warp->SetPosition(10.0, 0.0, -5.0);
+  warp->SetScaleFactor(1.0);
+  warp->AbsoluteOn();
+
+  vtkPolyDataNormals *polyNormals = vtkPolyDataNormals::New();
+  polyNormals->SetInputConnection(warp->GetOutputPort());
+  polyNormals->SplittingOn();
+
+  vtkStripper *stripper = vtkStripper::New();
+  stripper->SetInputConnection(polyNormals->GetOutputPort());
+
+  stripper->Update();
+
+  vtkPolyData *leafData = static_cast<vtkPolyData *>(stripper->GetOutput());
+  vtkPoints *leafPoints = leafData->GetPoints();
+  vtkCellArray *leafStrips = leafData->GetStrips();
+  vtkDataArray *leafNormals = leafData->GetPointData()->GetNormals();
+
+  vtkPolyData *data = vtkPolyData::New();
+  points = vtkPoints::New();
+  vtkDoubleArray *normals = vtkDoubleArray::New();
+  normals->SetNumberOfComponents(3);
+  vtkCellArray *strips = vtkCellArray::New();
+  vtkIntArray *scalars = vtkIntArray::New();
+
+  vtkTransform *transform = vtkTransform::New();
+  transform->PostMultiply();
+
+  static double rotate90[16] = {
+    1, 0, 0, 0,
+    0, 0, 1, 0,
+    0,-1, 0, 0,
+    0, 0, 0, 1,
+  };
+
+  transform->Concatenate(rotate90);
+  transform->Scale(3.0, 3.0, 1.5);
+  transform->Translate(-30, 16, 0);
+                    
+  for (int j = 0; j < 2; j++)
+    {
+    vtkIdType nPoints = points->GetNumberOfPoints();
+    transform->TransformPoints(leafPoints, points);
+    transform->TransformNormals(leafNormals, normals);
+    vtkIdType npts;
+    vtkIdType *pts;
+    leafStrips->InitTraversal();
+    while (leafStrips->GetNextCell(npts, pts))
+      {
+      strips->InsertNextCell(npts);
+      for (vtkIdType k = 0; k < npts; k++)
+        {
+        strips->InsertCellPoint(pts[k] + nPoints);
+        }
+      }
+    vtkIdType n = leafPoints->GetNumberOfPoints();
+    for (int ii = 0; ii < n; ii++)
+      {
+      scalars->InsertNextTupleValue(&color);
+      }
+    transform->Scale(-1, -1, 1);
+    color = !color;
+    }
+  transform->Delete();
+
+  data->SetPoints(points);
+  points->Delete();
+  data->GetPointData()->SetNormals(normals);
+  normals->Delete();
+  data->GetPointData()->SetScalars(scalars);
+  scalars->Delete();
+  data->SetStrips(strips);
+  strips->Delete();
+
+  polyNormals->Delete();
+  stripper->Delete();
+  warp->Delete();
+  reverse->Delete();
+  contour->Delete();
+  modeller->Delete();
+
+  return data;
 }
 
 //----------------------------------------------------------------------------
